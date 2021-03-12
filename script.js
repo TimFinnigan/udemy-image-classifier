a4ccb9b45481388c531532baaddef249597c4116
TimFinnigan <tim.finnigan1@gmail.com>
2021-03-11 17:41:54 -0800

    load files

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ async function init() {
 	model = await tmImage.load(modelURL, metadataURL);
 }
 
+// Load filepaths from JSON file
+function loadFiles() {
+	return fetch('../json/files.json')
+		.then((response) => response.json())
+		.then((images) => {
+			return images;
+		});
+}
+
 // Main thread
 init().then(() => {
 	document.getElementById('loading-message').style.display = 'none';
+
+	loadFiles().then((images) => {
+		// addDomElements(images);
+		console.log(images);
+	});
 });
