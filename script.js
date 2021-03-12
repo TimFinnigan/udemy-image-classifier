f029c23907558b704b2b4e657c2ea3e0f77e7567
TimFinnigan <tim.finnigan1@gmail.com>
2021-03-11 17:21:21 -0800

    load model

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('test');
+let model;
+
+// Load model
+async function init() {
+	const modelURL = './model/model.json';
+	const metadataURL = './model/metadata.json';
+
+	model = await tmImage.load(modelURL, metadataURL);
+}
+
+// Main thread
+init().then(() => {
+	document.getElementById('loading-message').style.display = 'none';
+});
